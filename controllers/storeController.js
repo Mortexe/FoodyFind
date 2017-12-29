68c9d1cc40bfc14c65d4cb02638a36d4271fe268
Mortexe <paris.chrysanthou@gmail.com>
2017-12-29 21:13:24 +0200

    DB query - Stores Displayed

--- controllers/storeController.js
+++ controllers/storeController.js
@@ -16,3 +16,10 @@ exports.createStore = async (req, res) => {
     req.flash('success', `Successfully Created ${store.name}. Care to leave a review?`);
     res.redirect(`/store/${store.slug}`);
 };
+
+exports.getStores = async (req,res) => {
+  // 1. Query the DB for a list of all stores
+  const stores = await Store.find();
+  console.log(stores);
+  res.render('stores', { title: 'Stores', stores});
+};
